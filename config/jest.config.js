63b12154573f0dbf12c837154afcd142c63c43d3
Osei Bonsu <osei.bonsu@gmail.com>
2018-12-20 11:48:54 -0600

    first commit

---
+++ config/jest.config.js
@@ -0,0 +1,44 @@
+module.exports = {
+  rootDir: '../',
+  transform: {
+    '.*': 'babel-jest',
+  },
+  moduleFileExtensions: [
+    'js',
+    'jsx',
+    'json',
+  ],
+  moduleDirectories: [
+    'node_modules',
+    'src',
+    './',
+  ],
+  moduleNameMapper: {
+    '^.+\\.(css|scss)$': '<rootDir>/test/__setup__/styleMock.js',
+    '^(.+\\.(jpe?g|png|gif|ttf|eot|svg|md)|bootstrap.*)$': '<rootDir>/test/__setup__/fileMock.js',
+  },
+  setupFiles: [
+    '<rootDir>/test/__setup__/shim.js',
+    '<rootDir>/test/__setup__/index.js',
+  ],
+  setupTestFrameworkScriptFile: 'jest-enzyme/lib/index.js',
+  testEnvironment: 'jest-environment-jsdom-global',
+  testEnvironmentOptions: {
+    resources: 'usable',
+  },
+  testRegex: '/test/.*?\\.(test|spec)\\.js$',
+  testURL: 'http://localhost:3000',
+  collectCoverage: false,
+  collectCoverageFrom: [
+    'src/scripts/**/*.{js,jsx}',
+  ],
+  coverageThreshold: {
+    global: {
+      branches: 65,
+      functions: 65,
+      lines: 65,
+      statements: 65
+    },
+  },
+  verbose: true,
+};
---
+++ test/__setup__/index.js
@@ -0,0 +1,30 @@
+import Enzyme from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+
+import './polyfills';
+
+Enzyme.configure({ adapter: new Adapter() });
+
+Object.defineProperty(Element.prototype, 'clientHeight', {
+  writable: true,
+  value: '',
+});
+
+Object.defineProperty(Element.prototype, 'clientWidth', {
+  writable: true,
+  value: '',
+});
+
+const react = document.createElement('div');
+react.id = 'react';
+react.style.height = '100vh';
+document.body.appendChild(react);
+
+window.matchMedia = () =>
+  ({
+    matches: false,
+    addListener: () => {
+    },
+    removeListener: () => {
+    },
+  });
